374d3da803bd187d078cb1c5fcb68ddd7829a3a2
LibraAlpha <yangchenhao8909@gmail.com>
2023-08-17 14:20:35 +0800

    调整spark脚本

--- scripts/scan.py
+++ scripts/scan.py
@@ -20,23 +20,29 @@ class Scanner(object):
         self.spark = SparkSession.builder.enableHiveSupport().getOrCreate()
         self.sparkContext = self.spark.sparkContext
 
-    def write_to_mysql(self, iterator):
+    def write_to_mysql(self, iterator, dt):
         count = 0
         session = mysql_tools.sessionmaker(bind=mysql_tools.engine)
-        for row in iterator:
-            asset = Asset()
-            asset.url = row.img_url
-            asset.title = row.title
-            asset.description = row.description
-            asset.adv = row.adv
-            asset.ad_pos = row.ad_pos
-            asset.url_md5 = get_md5_hash(asset_url)
-            asset.created_at = row.dt
+        try:
+            for row in iterator:
 
-            count += 1
+                asset = Asset()
+                asset.url = row['img_url']
+                asset.title = row['title']
+                asset.description = row['description']
+                asset.adv = row['adv']
+                asset.ad_pos = row['ad_pos']
+                asset.url_md5 = get_md5_hash(asset_url)
+                asset.created_at = dt
 
-            if count % 1000 == 0:
-                session.commit()
+                count += 1
+
+                if count % 1000 == 0:
+                    session.commit()
+        except Exception as e:
+            print(e)
+        finally:
+            session.close()
 
     def scan(self, date):
         get_bid_data = f"""
@@ -46,28 +52,28 @@ class Scanner(object):
             get_json_object(kafka_value, '$.AD') as description,
             get_json_object(kafka_value, '$.I') as adpos,
             get_json_object(kafka_value, '$.C') as adv,
-            1 as bid            
+            count(1) as bid            
             from wiseadx.ods_data_mor_ro
             where dt = '{date}'           
-            and type='B'
+            and t='B'
             and get_json_object(kafka_value, '$.IUL') is not null
-            and h = '3'            
+            group by id, img_url, title, description, adpos, adv                       
         """
 
         get_impression_data = f"""
-          select get_json_object(kafka_value, '$.id') as id, 1 as impression                        
+          select get_json_object(kafka_value, '$.id') as id, count(1) as impression                        
             from wiseadx.ods_data_mor_ro
             where dt = '{date}'           
-            and type='R'
-            and h = '3'   
+            and t='R'  
+            group by get_json_object(kafka_value, '$.id')            
         """
 
         get_click_data = f"""
-            select get_json_object(kafka_value, '$.id') as id, 1 as click                        
+            select get_json_object(kafka_value, '$.id') as id, count(1) as click                        
             from wiseadx.ods_data_mor_ro
             where dt = '{date}'           
-            and type='C'
-            and h = '3'
+            and t='C'
+            group by get_json_object(kafka_value, '$.id')      
         """
 
         bid_data = self.spark.sql(get_bid_data).drop_duplicates().repartition(2000).fillna('None')
@@ -82,7 +88,7 @@ class Scanner(object):
             sum(col('click')).cast('int').alias('clicks')
         )
 
-        stats.write.mode('overwrite') \
+        stats.repartition(10).write.mode('overwrite') \
             .option('header', 'true') \
             .csv('hdfs://hdfscluster/user/hive/warehouse/test.db/export/tag=asset_report.csv')
 
